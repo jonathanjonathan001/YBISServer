90af2fe86faae40e9bdde543e81a5ec494b46516
Jonathan Råsmark <jonathanrosemark@gmail.com>
2024-02-13 14:37:37 +0100

    Initial commit

---
+++ src/main/java/com/jonfen/ybisserver/YbisServerApplication.java
@@ -0,0 +1,13 @@
+package com.jonfen.ybisserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YbisServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YbisServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jonfen/ybisserver/YbisServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonfen.ybisserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YbisServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
